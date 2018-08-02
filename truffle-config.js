437fe335c8c9535bb35ac4562d66e922eb5863bc
saguywalker <guyguy252@gmail.com>
2018-08-03 01:29:18 +0700

    deploy contracts

---
+++ truffle-config.js
@@ -0,0 +1,11 @@
+module.expoerts = {
+  networks:{
+    localnode:{
+      network_id:"*",
+      host:"localhost",
+      port:"8545",
+      gas:4700000,
+      gasPrice:100000000000,
+    }
+  }
+}
