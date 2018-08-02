42620caf353aec9a6a6e28bd3fb7d498d0c26fb9
saguywalker <guyguy252@gmail.com>
2018-08-03 00:11:25 +0700

    add withdrawal and deposit events

--- Faucet.sol
+++ Faucet.sol
@@ -2,12 +2,18 @@ pragma solidity ^0.4.22;
 
 contract Faucet is Mortal{
 
+  event Withdrawal(address indexed to, uint amount);
+  event Deposit(address indexed from, uint amout);
+
   function withdraw(uint withdraw_amount) public{
     require(withdraw_amount <= 100000000000000000);
     require(this.balance >= withdraw_amount,"Insufficient balance for withdraw request");
     msg.sender.transfer(withdraw_amount);
+    emit Withdrawal(msg.sender,msg.value);
   }
 
-  function () public payable{}
+  function () public payable{
+    emit Deposit(msg.sender,msg.value);
+  }
 
 }
