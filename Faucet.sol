1ca66d0ffc6d2aa833512cbc66aecdb801a38651
saguywalker <guyguy252@gmail.com>
2018-08-03 00:59:58 +0700

    merge to Faucet.sol

--- Faucet.sol
+++ Faucet.sol
@@ -17,3 +17,23 @@ contract Faucet is Mortal{
   }
 
 }
+
+contract Mortal is Owned{
+  function destroy() public onlyOwner{
+    selfdestruct(owner);
+  }
+}
+
+contract Owned{
+
+  address owner;
+
+  constructor(){
+    msg.sender = owner;
+  }
+
+  modifier onlyOwner(){
+    require(msg.sender == owner,"only the contract owner can call this function");
+    _;
+  }
+}
--- Mortal.sol
+++
@@ -1,5 +0,0 @@
-contract Mortal is Owned{
-  function destroy() public onlyOwner{
-    selfdestruct(owner);
-  }
-}
