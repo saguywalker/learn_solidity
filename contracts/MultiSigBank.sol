c35d172f3a42c38c0f6ea0b3d175409b981afc77
saguywalker <guyguy252@gmail.com>
2019-02-01 14:56:07 +0700

    add createParty event

--- contracts/MultiSigBank.sol
+++ contracts/MultiSigBank.sol
@@ -15,8 +15,9 @@ contract MultiSigBank{
 
     mapping(bytes32 => Party) partyId;
 
-    event Deposit(address indexed person1, address indexed person2, uint256 amount);
-    event Withdraw(address indexed person1, address indexed person2, uint256 person1Amount, uint256 person2Amount);
+    event CreateParty(bytes32 indexed partyId, address person1, address person2, uint256 amount);
+    event Deposit(bytes32 indexed partyId, uint256 amount);
+    event Withdraw(bytes32 indexed partyId, uint256 person1Amount, uint256 person2Amount);
 
     function createParty(address payable _person2) external payable{
         require(msg.value > 0, "must greater than 0.");
